c482a56b84d073975db1f6996e098f9375444448
wangjifang <wangjifang@wsn.cn>
2017-04-06 16:44:42 +0800

    out_0406_modify:增加条件语句，循环语句，while，for

--- test.py
+++ test.py
@@ -125,3 +125,59 @@ if (num >= 0 and num <= 5) or (num >= 10 and num <= 15):
 else:
     print 'undefine'
 
+count = 0
+while (count < 9):
+   print 'The count is:', count
+   count = count + 1
+
+print "Good bye!"
+
+i = 1
+while i < 10:   
+    i += 1
+    if i%2 > 0:     # 非双数时跳过输出
+        continue
+    print i         # 输出双数2、4、6、8、10
+
+i = 1
+while 1:            # 循环条件为1必定成立
+    print i         # 输出1~10
+    i += 1
+    if i > 10:     # 当i大于10时跳出循环
+        break
+
+# var = 1
+# while var == 1 :  # 该条件永远为true，循环将无限执行下去
+#    num = raw_input("Enter a number  :")
+#    print "You entered: ", num
+
+# print "Good bye!"
+# 
+count = 0
+while count < 5:
+   print count, " is  less than 5"
+   count = count + 1
+else:
+   print count, " is not less than 5"
+
+for letter in 'Python':     # 第一个实例
+   print '当前字母 :', letter
+ 
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:        # 第二个实例
+   print '当前水果 :', fruit
+ 
+print "Good bye!"
+
+for num in range(10,20):  # 迭代 10 到 20 之间的数字
+   for i in range(2,num): # 根据因子迭代
+      if num%i == 0:      # 确定第一个因子
+         j=num/i          # 计算第二个因子
+         print '%d 等于 %d * %d' % (num,i,j)
+         break            # 跳出当前循环
+   else:                  # 循环的 else 部分
+      print num, '是一个质数'
+
+sequence = [12, 34, 34, 23, 45, 76, 89]
+for i, j in enumerate(sequence):
+    print i,j
